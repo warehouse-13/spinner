a5d0001fc2af2df06832b1dc7e21633d7180e5bc
nikimanoledaki <niki@weave.works>
2022-03-02 16:13:34 +0100

    Updating to use goroutine

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -5,14 +5,29 @@ import (
 	"net/http"
 )
 
+var quit chan struct{}
+
 func Spin(http.ResponseWriter, *http.Request) {
-	log.Println("starting")
-	for {
-		for i := 0; i < 100000000; i++ {
+	quit = (make(chan struct{}))
+	log.Println("starting to spin")
+	go func() {
+	loop:
+		for {
+			select {
+			case <-quit:
+				log.Println("received signal to stop")
+				break loop
+			default:
+				for i := 0; i < 3; i++ {
+				}
+			}
 		}
-	}
+	}()
+
+	log.Println("stopped")
 }
 
 func Unspin(http.ResponseWriter, *http.Request) {
-	log.Println("stopping")
+	log.Println("sending signal to stop")
+	close(quit)
 }
